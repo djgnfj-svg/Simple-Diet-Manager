11d4f57bab97d6f2c99ae56f4fec820a56f3fcc4
djgnfj-svg <djgnfj8923@naver.com>
2023-03-28 21:31:32 +0900

    add food-categoty master data

--- backend/Food/models.py
+++ backend/Food/models.py
@@ -1,21 +1,26 @@
 from django.db import models
 
+from Utils.model.Timestemp import TimeStampedModel
+
 # Create your models here.
-class FoodCategory(models.Model):
-    name = models.CharField(max_length=50)
+class FoodCategory(TimeStampedModel):
+    name = models.CharField(max_length=50, unique=True)
+
+class CookingOption(TimeStampedModel):
+    name = models.CharField(max_length=50, unique=True)
 
 
+class Food(TimeStampedModel):
+    name = models.CharField(max_length=50, unique=True)
+    kcal = models.IntegerField(null=False, default=0)
+    protein = models.IntegerField(null=False, default=0)
+    fat = models.IntegerField(null=False, default=0)
+    carbs = models.IntegerField(null=False, default=0)
 
-class Food(models.Model):
-    name = models.CharField(max_length=50)
-    kcal = models.IntegerField()
-    protein = models.IntegerField()
-    fat = models.IntegerField()
-    carbs = models.IntegerField()
+    number = models.IntegerField(null=False, default=0)
+    gram = models.IntegerField(null=False, default=0)
+    link = models.URLField(max_length=100, null=True)
+    img = models.ImageField(upload_to='food_img')
 
-    food_count = models.IntegerField()
-    food_link = models.URLField(max_length=100)
-    food_img = models.ImageField(upload_to='food_img')
-
-    food_category = models.ManyToManyField(FoodCategory, related_name="foods")
-    created_at = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(FoodCategory, on_delete=models.CASCADE, related_name="foods")
+    cooking = models.ForeignKey(CookingOption, on_delete=models.CASCADE, related_name="foods")
