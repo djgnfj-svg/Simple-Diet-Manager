487687da579c0a92081ddeedd3f4a0eb784b2eb1
djgnfj-svg <djgnfj8923@naver.com>
2023-04-06 20:25:37 +0900

    업데이트 식품 추가시에 영양소 및 이름 자동지정

--- backend/api/Serializer/MealSerializer.py
+++ backend/api/Serializer/MealSerializer.py
@@ -13,5 +13,43 @@ class MealSerializer(serializers.ModelSerializer):
         model = Meal
         fields = "__all__"
 
+    # admin을 사용할꺼면 이로직을 그대로 사용해야한다.
     def create(self, validated_data):
-        return super().create(validated_data)
+        instance = super().create(validated_data)
+        print(instance.foods.order_by('-protein')[0].category_id)
+        if instance.name is '':
+            # TODO : 카테고리로 이름을 정해도 된다.
+            if len(instance.foods.order_by('-protein')[0].name) < 5:
+                name = instance.foods.order_by('-protein')[0].name
+            else :
+                name = instance.foods.order_by('-protein')[0].name[:5]
+            instance.name = f"{name} 외 {instance.foods.count() - 1}개"
+
+        for food in instance.foods.all():
+            instance.meal_kcal += food.kcal
+            instance.meal_protein += food.protein
+            instance.meal_fat += food.fat
+            instance.meal_carbs += food.carbs
+        instance.save()
+        return instance
+    
+    def update(self, instance, validated_data):
+        instance = super().update(instance, validated_data)
+        instance.meal_kcal = 0
+        instance.meal_protein = 0
+        instance.meal_fat = 0
+        instance.meal_carbs = 0
+        if len(instance.foods.order_by('-protein')[0].name) < 5:
+            name = instance.foods.order_by('-protein')[0].name
+        else :
+            name = instance.foods.order_by('-protein')[0].name[:5]
+        instance.name = f"{name} 외 {instance.foods.count() - 1}개"
+        
+        for food in instance.foods.all():
+            instance.meal_kcal += food.kcal
+            instance.meal_protein += food.protein
+            instance.meal_fat += food.fat
+            instance.meal_carbs += food.carbs
+        instance.save()
+
+        return instance
--- backend/meals/admin.py
+++ backend/meals/admin.py
@@ -4,9 +4,9 @@ from meals.models import Meal
 # Register your models here.
 
 
-class MealAdmin(admin.ModelAdmin):
-    readonly_fields = ('meal_kcal', 'meal_protein', 'meal_fat', 'meal_carbs')
-    def save_model(self, request, obj, form, change) -> None:
-        return obj.save(isupdate=change, foods=form.cleaned_data['foods'])
+# class MealAdmin(admin.ModelAdmin):
+#     readonly_fields = ('meal_kcal', 'meal_protein', 'meal_fat', 'meal_carbs')
+#     def save_model(self, request, obj, form, change) -> None:
+#         return obj.save(isupdate=change, foods=form.cleaned_data['foods'])
     
-admin.site.register(Meal, MealAdmin)
+admin.site.register(Meal)
