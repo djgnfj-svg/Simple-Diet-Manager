cecb122bf96eb66aee47fef6686243a6822316e8
djgnfj-svg <djgnfj8923@naver.com>
2023-04-11 10:04:19 +0900

    수정 식품테스트 + github actions

--- backend/api/test/test_food.py
+++ backend/api/test/test_food.py
@@ -2,23 +2,21 @@ from rest_framework.test import APITestCase
 
 #  make test DRF food add api
 class FoodAddTest(APITestCase):
+    fixtures = ['_Master_data/Food-Category.json', '_Master_data/Cooking-Category.json']
     def setUp(self) -> None:
         self.url = '/api/foods/'
         self.food_data = {
             "name": "한끼통살 통살 닭가슴살 허니소이",
             "kcal": 135,
             "protein": 21,
-            "fat": 1.5,
+            "fat": 12,
             "carbs": 9,
             "number": 10,
             "gram": 100,
             "iscoupangfresh" : True,
-            "category" : 1,
-            "img" : "master_img/category/chicken-breast.jpg",
-            "cookingoption" : 5,
             "link": "https://www.coupang.com/vp/products/6081081115?itemId=11273250763",
-            "created_at": "2023-02-15",
-            "updated_at": "2023-02-15"
+            "category" : 1,
+            "cookingoption" : 5,
         }
         return super().setUp()
     
