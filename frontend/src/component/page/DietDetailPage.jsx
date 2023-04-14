2e3ceecf3100203e1e02bbbf733e7e1c7c504e64
djgnfj-svg <djgnfj8923@naver.com>
2023-04-15 01:35:17 +0900

    fix axios url

--- frontend/src/component/page/DietDetailPage.jsx
+++ frontend/src/component/page/DietDetailPage.jsx
@@ -18,7 +18,7 @@ function DietDetailPage() {
     const { id } = useParams();
     const [mealData, setMealData] = useState([]);
     useEffect(() => {
-        axios.get(`http://localhost:8000/api/diets/${id}/`)
+        axios.get(`${process.env.PUBLIC_URL}/api/diets/${id}/`)
         .then(res => {
             const data = res.data;
             setMealData([data.meals]);
